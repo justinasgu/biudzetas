239f8581343d04b4f0c89a0f76c8f5b840dfbd4b
justinasgu <guzys.justinas@gmail.com>
2022-12-15 15:02:50 +0200

    prideti failai

---
+++ biudzetas/pajamu_irasas.py
@@ -0,0 +1,6 @@
+from biudzetas.irasas import Irasas
+class PajamuIrasas(Irasas):
+    def __init__(self, suma, siuntejas, papildoma_info):
+        super().__init__(suma)
+        self.siuntejas = siuntejas
+        self.papildoma_info = papildoma_info
